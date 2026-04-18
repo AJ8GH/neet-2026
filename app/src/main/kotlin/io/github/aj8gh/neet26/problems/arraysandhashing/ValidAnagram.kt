6f90c900073600734963f14ff7c6cff2f1f8fb18
Adam Jonas <adamjonas25@gmail.com>
2026-04-18 09:48:13 +0100

    Solve valid anagram

--- app/src/main/kotlin/io/github/aj8gh/neet26/problems/arraysandhashing/ValidAnagram.kt
+++ app/src/main/kotlin/io/github/aj8gh/neet26/problems/arraysandhashing/ValidAnagram.kt
@@ -1,5 +1,15 @@
 package io.github.aj8gh.neet26.problems.arraysandhashing
 
 fun isAnagram(s: String, t: String): Boolean {
-  return false
+  if (s.length != t.length) {
+    return false
+  }
+  val count = mutableMapOf<Char, Int>()
+  for (c in s) {
+    count[c] = count.getOrDefault(c, 0) + 1
+  }
+  for (c in t) {
+    count[c] = count.getOrDefault(c, 0) - 1
+  }
+  return count.all { it.value == 0 }
 }
