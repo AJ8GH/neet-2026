07152493b1357067f6e4419b3808b2145e58758e
Adam Jonas <adamjonas25@gmail.com>
2026-04-30 22:02:29 +0100

    Solve daily temperatures

--- app/src/main/kotlin/io/github/aj8gh/neet26/problems/stack/DailyTemperatures.kt
+++ app/src/main/kotlin/io/github/aj8gh/neet26/problems/stack/DailyTemperatures.kt
@@ -1,5 +1,14 @@
 package io.github.aj8gh.neet26.problems.stack
 
 fun dailyTemperatures(temperatures: IntArray): IntArray {
-  return intArrayOf()
+  val output = IntArray(temperatures.size)
+  val stack = ArrayDeque<Pair<Int, Int>>()
+  for ((i, temp) in temperatures.withIndex()) {
+    while (stack.isNotEmpty() && stack.last().second < temp) {
+      val last = stack.removeLast()
+      output[last.first] = i - last.first
+    }
+    stack.add(i to temp)
+  }
+  return output
 }
