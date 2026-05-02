9b6f03810358584de79b4e14bd0c6c029c901666
Adam Jonas <adamjonas25@gmail.com>
2026-05-02 22:19:38 +0100

    Solve car fleet

--- app/src/main/kotlin/io/github/aj8gh/neet26/problems/stack/CarFleet.kt
+++ app/src/main/kotlin/io/github/aj8gh/neet26/problems/stack/CarFleet.kt
@@ -1,5 +1,20 @@
 package io.github.aj8gh.neet26.problems.stack
 
 fun carFleet(target: Int, position: IntArray, speed: IntArray): Int {
-  return 0
+  val sorted = position.withIndex()
+    .map { (i, p) -> Pair(p, speed[i]) }
+    .sortedByDescending { it.first }
+
+  var fleets = 0
+  var max = 0.0
+
+  for (p in sorted) {
+    val time = (target - p.first).toDouble() / p.second
+    if (time > max) {
+      fleets++
+      max = time
+    }
+  }
+
+  return fleets
 }
--- app/src/test/kotlin/io/github/aj8gh/neet26/problems/stack/CarFleetTest.kt
+++ app/src/test/kotlin/io/github/aj8gh/neet26/problems/stack/CarFleetTest.kt
@@ -7,7 +7,10 @@ import io.kotest.matchers.shouldBe
 
 class CarFleetTest : FunSpec({
   withTests(
+    Tuple4(10, intArrayOf(8, 3, 7, 4, 6, 5), intArrayOf(4, 4, 4, 4, 4, 4), 6),
+    Tuple4(20, intArrayOf(6, 2, 17), intArrayOf(3, 9, 2), 2),
     Tuple4(10, intArrayOf(1, 4), intArrayOf(3, 2), 1),
+    Tuple4(12, intArrayOf(10, 8, 0, 5, 3), intArrayOf(2, 4, 1, 1, 3), 3),
     Tuple4(10, intArrayOf(4, 1, 0, 7), intArrayOf(2, 2, 1, 1), 3),
   ) { (target, position, speed, expected) ->
     carFleet(target, position, speed) shouldBe expected
