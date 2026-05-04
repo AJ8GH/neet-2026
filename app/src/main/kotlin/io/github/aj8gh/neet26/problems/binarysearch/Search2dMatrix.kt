d4e95bb14ee30bba684ab0eb6eed7bdf7b617503
Adam Jonas <adamjonas25@gmail.com>
2026-05-04 08:13:33 +0100

    Solve search 2d matrix

--- app/src/main/kotlin/io/github/aj8gh/neet26/problems/binarysearch/Search2dMatrix.kt
+++ app/src/main/kotlin/io/github/aj8gh/neet26/problems/binarysearch/Search2dMatrix.kt
@@ -1,5 +1,32 @@
 package io.github.aj8gh.neet26.problems.binarysearch
 
 fun searchMatrix(matrix: Array<IntArray>, target: Int): Boolean {
+  var start = 0
+  var end = matrix.lastIndex
+
+  while (start <= end) {
+    val pivot = (end - start) / 2 + start
+    val row = matrix[pivot]
+    when {
+      target in row.first()..row.last() -> return searchRow(row, target)
+      target < row.first() -> end = pivot - 1
+      target > row.last() -> start = pivot + 1
+    }
+  }
+  return false
+}
+
+private fun searchRow(row: IntArray, target: Int): Boolean {
+  var start = 0
+  var end = row.lastIndex
+
+  while (start <= end) {
+    val pivot = (end - start) / 2 + start
+    when {
+      row[pivot] == target -> return true
+      row[pivot] < target -> start = pivot + 1
+      row[pivot] > target -> end = pivot - 1
+    }
+  }
   return false
 }
