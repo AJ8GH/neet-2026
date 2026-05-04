203b5808ec0b08691cdbf4a30dbdd979834820ec
Adam Jonas <adamjonas25@gmail.com>
2026-05-04 22:56:20 +0100

    Solve kolo eating bananas

--- app/src/main/kotlin/io/github/aj8gh/neet26/problems/binarysearch/KokoEatingBananas.kt
+++ app/src/main/kotlin/io/github/aj8gh/neet26/problems/binarysearch/KokoEatingBananas.kt
@@ -1,5 +1,29 @@
 package io.github.aj8gh.neet26.problems.binarysearch
 
+import kotlin.Int.Companion.MAX_VALUE
+
 fun minEatingSpeed(piles: IntArray, h: Int): Int {
-  return 0
+  var minK = MAX_VALUE
+  var lowerBound = 1
+  var upperBound = piles.maxOrNull()!!
+  while (lowerBound <= upperBound) {
+    val k = (upperBound - lowerBound) / 2 + lowerBound
+    if (canEatAll(piles, h, k)) {
+      minK = k
+      upperBound = k - 1
+    } else {
+      lowerBound = k + 1
+    }
+  }
+
+  return minK
+}
+
+fun canEatAll(piles: IntArray, h: Int, k: Int): Boolean {
+  var hours = h
+  for (p in piles) {
+    hours -= (p + k - 1) / k
+    if (hours < 0) return false
+  }
+  return true
 }
