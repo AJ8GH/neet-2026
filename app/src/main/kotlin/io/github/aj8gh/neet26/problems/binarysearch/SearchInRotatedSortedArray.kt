0ad3c81a136613afa391ff0369e275f179525872
Adam Jonas <adamjonas25@gmail.com>
2026-05-08 21:10:18 +0100

    Refactor

--- app/src/main/kotlin/io/github/aj8gh/neet26/problems/binarysearch/SearchInRotatedSortedArray.kt
+++ app/src/main/kotlin/io/github/aj8gh/neet26/problems/binarysearch/SearchInRotatedSortedArray.kt
@@ -2,28 +2,20 @@ package io.github.aj8gh.neet26.problems.binarysearch
 
 class SearchInRotatedSortedArray {
   fun search(nums: IntArray, target: Int): Int {
-    val rotation = findRotation(nums)
     var lowerBound = 0
     var upperBound = nums.lastIndex
     while (lowerBound <= upperBound) {
       val pivot = (upperBound - lowerBound) / 2 + lowerBound
-      val rotatedPivot = (pivot + rotation) % nums.size
-      if (nums[rotatedPivot] == target) return rotatedPivot
-      if (nums[rotatedPivot] < target) lowerBound = pivot + 1
-      if (nums[rotatedPivot] > target) upperBound = pivot - 1
+      when {
+        nums[pivot] == target -> return pivot
+        nums[lowerBound] <= nums[pivot] ->
+          if (target in nums[lowerBound]..nums[pivot]) upperBound = pivot - 1
+          else lowerBound = pivot + 1
+        nums[pivot] < nums[upperBound] ->
+          if (target in nums[pivot]..nums[upperBound]) lowerBound = pivot + 1
+          else upperBound = pivot - 1
+      }
     }
     return -1
-  }
-
-  @Suppress("DuplicatedCode")
-  private fun findRotation(nums: IntArray): Int {
-    var lowerBound = 0
-    var upperBound = nums.lastIndex
-    while (lowerBound < upperBound) {
-      val pivot = (upperBound - lowerBound) / 2 + lowerBound
-      if (nums[pivot] > nums[upperBound]) lowerBound = pivot + 1
-      else upperBound = pivot
-    }
-    return lowerBound
   }
 }
--- app/src/test/kotlin/io/github/aj8gh/neet26/problems/binarysearch/SearchInRotatedSortedArrayTest.kt
+++ app/src/test/kotlin/io/github/aj8gh/neet26/problems/binarysearch/SearchInRotatedSortedArrayTest.kt
@@ -6,6 +6,8 @@ import io.kotest.matchers.shouldBe
 
 class SearchInRotatedSortedArrayTest : FunSpec({
   withTests(
+    Triple(intArrayOf(3, 1), 0, -1),
+    Triple(intArrayOf(1, 3), 3, 1),
     Triple(intArrayOf(3, 4, 5, 6, 1, 2), 1, 4),
     Triple(intArrayOf(3, 5, 6, 0, 1, 2), 4, -1)
   ) { (nums, target, expected) ->
