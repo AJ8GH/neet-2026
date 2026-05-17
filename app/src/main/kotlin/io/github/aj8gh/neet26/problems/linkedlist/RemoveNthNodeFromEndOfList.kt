399af942fd8457317e3c28bb1b29abe241c3647f
Adam Jonas <adamjonas25@gmail.com>
2026-05-17 15:47:23 +0100

    Solve remove nth from end of list

--- app/src/main/kotlin/io/github/aj8gh/neet26/problems/linkedlist/RemoveNthNodeFromEndOfList.kt
+++ app/src/main/kotlin/io/github/aj8gh/neet26/problems/linkedlist/RemoveNthNodeFromEndOfList.kt
@@ -3,5 +3,15 @@ package io.github.aj8gh.neet26.problems.linkedlist
 import io.github.aj8gh.neet26.model.ListNode
 
 fun removeNthFromEnd(head: ListNode<Int>?, n: Int): ListNode<Int>? {
-  return null
+  head ?: return null
+  var prev = head
+  var end = head
+  repeat(n) { end = end?.next }
+  end ?: return head.next
+  while (end?.next != null) {
+    end = end.next
+    prev = prev!!.next
+  }
+  prev!!.next = prev.next!!.next
+  return head
 }
--- app/src/test/kotlin/io/github/aj8gh/neet26/problems/linkedlist/RemoveNthNodeFromEndOfListTest.kt
+++ app/src/test/kotlin/io/github/aj8gh/neet26/problems/linkedlist/RemoveNthNodeFromEndOfListTest.kt
@@ -8,9 +8,12 @@ import io.kotest.matchers.shouldBe
 
 class RemoveNthNodeFromEndOfListTest : FunSpec({
   withTests(
+    Triple(linkedListOf(1, 2, 3), 2, linkedListOf(1, 3)),
+    Triple(linkedListOf(1, 2), 1, linkedListOf(1)),
     Triple(linkedListOf(1, 2, 3, 4), 2, linkedListOf(1, 2, 4)),
+    Triple(linkedListOf(1, 2, 3, 4), 4, linkedListOf(2, 3, 4)),
+    Triple(linkedListOf(1, 2), 2, linkedListOf(2)),
     Triple(linkedListOf(5), 1, emptyLinkedList<Int>()),
-    Triple(linkedListOf(1, 2), 2, linkedListOf(1)),
   ) { (head, n, expected) ->
     removeNthFromEnd(head, n) shouldBe expected
   }
