003f06ae7a2f2daab42290149347e70c21e2db22
Adam Jonas <adamjonas25@gmail.com>
2026-06-01 20:35:31 +0100

    Solve binary tree level order traversal

--- app/src/main/kotlin/io/github/aj8gh/neet26/problems/trees/BinaryTreeLevelOrderTraversal.kt
+++ app/src/main/kotlin/io/github/aj8gh/neet26/problems/trees/BinaryTreeLevelOrderTraversal.kt
@@ -3,5 +3,17 @@ package io.github.aj8gh.neet26.problems.trees
 import io.github.aj8gh.neet26.model.TreeNode
 
 fun levelOrder(root: TreeNode<Int>?): List<List<Int>> {
-  TODO()
+  val levels = mutableListOf<List<Int>>()
+  val nodes = ArrayDeque(listOfNotNull(root))
+  while (nodes.isNotEmpty()) {
+    val level = mutableListOf<Int>()
+    repeat(nodes.size) {
+      val node = nodes.removeFirst()
+      level.add(node.value)
+      node.left?.let { nodes.add(it) }
+      node.right?.let { nodes.add(it) }
+    }
+    levels.add(level)
+  }
+  return levels
 }
