ee1728b073cc50382fa7df8d614a366645d20792
Adam Jonas <adamjonas25@gmail.com>
2026-06-24 21:42:14 +0100

    Solve valid binary search tree

--- app/src/main/kotlin/io/github/aj8gh/neet26/problems/trees/ValidBinarySearchTree.kt
+++ app/src/main/kotlin/io/github/aj8gh/neet26/problems/trees/ValidBinarySearchTree.kt
@@ -3,5 +3,24 @@ package io.github.aj8gh.neet26.problems.trees
 import io.github.aj8gh.neet26.model.TreeNode
 
 fun isValidBST(root: TreeNode<Int>?): Boolean {
-  TODO()
+  root ?: return true
+  return isValidBST(root.left, null, root.value)
+      && isValidBST(root.right, root.value, null)
+}
+
+private fun isValidBST(
+  root: TreeNode<Int>?,
+  leftParent: Int?,
+  rightParent: Int?,
+): Boolean {
+  root ?: return true
+
+  if (leftParent != null && leftParent >= root.value) return false
+  if (rightParent != null && rightParent <= root.value) return false
+
+  val rightParentForLeft = rightParent?.let { minOf(root.value, it) } ?: root.value
+  val leftParentForRight = leftParent?.let { maxOf(root.value, it) } ?: root.value
+
+  return isValidBST(root.left, leftParent, rightParentForLeft)
+      && isValidBST(root.right, leftParentForRight, rightParent)
 }
