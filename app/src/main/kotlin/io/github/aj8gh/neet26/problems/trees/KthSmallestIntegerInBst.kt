fcc3d58d243040c76d631abfb9326bf009a60eb4
Adam Jonas <adamjonas25@gmail.com>
2026-06-26 23:41:16 +0100

    Solve kth smallest integer in BST

--- app/src/main/kotlin/io/github/aj8gh/neet26/problems/trees/KthSmallestIntegerInBst.kt
+++ app/src/main/kotlin/io/github/aj8gh/neet26/problems/trees/KthSmallestIntegerInBst.kt
@@ -3,5 +3,22 @@ package io.github.aj8gh.neet26.problems.trees
 import io.github.aj8gh.neet26.model.TreeNode
 
 fun kthSmallest(root: TreeNode<Int>?, k: Int): Int {
-  TODO()
+  val nodes = ArrayDeque<TreeNode<Int>>()
+  queueAllLeft(root, nodes)
+  var i = 1
+  while (nodes.isNotEmpty()) {
+    val n = nodes.removeLast()
+    if (i == k) return n.value
+    queueAllLeft(n.right, nodes)
+    i++
+  }
+  error("Not found")
+}
+
+private fun queueAllLeft(n: TreeNode<Int>?, q: ArrayDeque<TreeNode<Int>>) {
+  var current = n
+  while (current != null) {
+    q.addLast(current)
+    current = current.left
+  }
 }
