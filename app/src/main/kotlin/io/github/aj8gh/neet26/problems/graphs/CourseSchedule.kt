bb90f10dbcbb2e34b98b50067abca6af77a369ba
Adam Jonas <adamjonas25@gmail.com>
2026-07-11 08:21:32 +0100

    Solve course schedule

--- app/src/main/kotlin/io/github/aj8gh/neet26/problems/graphs/CourseSchedule.kt
+++ app/src/main/kotlin/io/github/aj8gh/neet26/problems/graphs/CourseSchedule.kt
@@ -1,5 +1,27 @@
 package io.github.aj8gh.neet26.problems.graphs
 
 fun canFinish(numCourses: Int, prerequisites: Array<IntArray>): Boolean {
-  TODO()
+  val counts = IntArray(numCourses)
+  val dependentsByPrereq = mutableMapOf<Int, MutableList<Int>>()
+  val queue = ArrayDeque<Int>()
+
+  for ((course, prereq) in prerequisites) {
+    counts[course]++
+    val list = dependentsByPrereq.getOrPut(prereq) { mutableListOf() }
+    list.add(course)
+  }
+
+  for ((i, c) in counts.withIndex()) {
+    if (c == 0) queue.addLast(i)
+  }
+
+  while (queue.isNotEmpty()) {
+    val course = queue.removeFirst()
+    val dependents = dependentsByPrereq[course] ?: emptyList()
+    for (d in dependents) {
+      counts[d]--
+      if (counts[d] == 0) queue.add(d)
+    }
+  }
+  return counts.all { it == 0 }
 }
--- app/src/test/kotlin/io/github/aj8gh/neet26/problems/graphs/CourseScheduleTest.kt
+++ app/src/test/kotlin/io/github/aj8gh/neet26/problems/graphs/CourseScheduleTest.kt
@@ -6,7 +6,51 @@ import io.kotest.matchers.shouldBe
 
 class CourseScheduleTest : FunSpec({
   withTests(
+    Triple(
+      4,
+      arrayOf(
+        intArrayOf(2, 0),
+        intArrayOf(1, 0),
+        intArrayOf(3, 1),
+        intArrayOf(3, 2),
+        intArrayOf(1, 3)
+      ),
+      false
+    ),
+    Triple(3, arrayOf(intArrayOf(1, 0), intArrayOf(2, 1)), true),
     Triple(2, arrayOf(intArrayOf(0, 1)), true),
+    Triple(
+      3,
+      arrayOf(
+        intArrayOf(1, 0),
+        intArrayOf(1, 2),
+        intArrayOf(0, 1)
+      ),
+      false
+    ),
+    Triple(
+      3,
+      arrayOf(
+        intArrayOf(1, 0),
+        intArrayOf(0, 2),
+        intArrayOf(2, 1)
+      ),
+      false
+    ),
+    Triple(
+      20,
+      arrayOf(
+        intArrayOf(0, 10),
+        intArrayOf(3, 18),
+        intArrayOf(5, 5),
+        intArrayOf(6, 11),
+        intArrayOf(11, 14),
+        intArrayOf(13, 1),
+        intArrayOf(15, 1),
+        intArrayOf(17, 4)
+      ),
+      false
+    ),
     Triple(2, arrayOf(intArrayOf(0, 1), intArrayOf(1, 0)), false),
   ) { (numCourses, prerequisites, expected) ->
     canFinish(numCourses, prerequisites) shouldBe expected
